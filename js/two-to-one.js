8bc097af630e8f7681a98bef75201e409957a83e
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-23 11:50:44 +0100

    Update two-to-one.js
    fixed a typo

--- js/two-to-one.js
+++ js/two-to-one.js
@@ -6,7 +6,7 @@ function longest(s1, s2) {
     
     if(typeof s1 === 'string' && typeof s2 === 'string'){
       joinedString = s1.concat(s2).split('').sort()
-      filteredString = joinedString.filter((item, index, array) => index === array.indexOf(item) ) //indexOf a;ways stops at the first item it encounters that is true, that's why it can be used here
+      filteredString = joinedString.filter((item, index, array) => index === array.indexOf(item) ) //indexOf always stops at the first item it encounters that is true, that's why it can be used here
     }
     console.log(filteredString)
   }
