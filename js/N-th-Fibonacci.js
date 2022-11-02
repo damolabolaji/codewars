ed5f5c5de6db40e3af6f003b9cd8745ceb917a1b
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-11-02 08:46:04 +0100

    Update N-th-Fibonacci.js
    added alternative solution

--- js/N-th-Fibonacci.js
+++ js/N-th-Fibonacci.js
@@ -29,3 +29,14 @@ function nthFibo(n) {
   for (let i = 1; i < n; i++) [prev, curr] = [curr, prev + curr];
   return prev;
 }
+
+function nthFibo(n) {
+  let a = 0, b = 1;
+  
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return n == 1? a: b;
+}
